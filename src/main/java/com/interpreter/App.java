204ee46290663ff8f816fba146aad5ddc8dfd729
mpetrov19 <michael.nekrasov75@gmail.com>
2014-04-12 10:22:04 +0400

    Create project

---
+++ src/main/java/com/interpreter/App.java
@@ -0,0 +1,13 @@
+package com.interpreter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
